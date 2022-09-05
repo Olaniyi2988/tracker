7c3fbd0372d3fb39cb03a1ae4837925ad0d8e9f8
Olaniyi2988 <psimeon360@gmail.com>
2022-09-05 12:33:53 +0100

    delete task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Task from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Hello',
+        day: 'feb 5th at 2:30',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'World',
+        day: 'feb 8th at 2:30',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Today',
+        day: 'feb 3th at 2:30',
+        reminder: true,
+      }
+    ]
+  )
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Task tasks={tasks} onDelete={deleteTask} />)
+        : ('No Task')}
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
-const Button = () => {
-  return (
-    <button className='btn'>Add</button>
-  )
+import PropTypes from 'prop-types'
+
+
+const Button = ({ color, text, onClick }) => {
+  return <button style={{ backgroundColor: color }} className='btn' onClick={onClick}>{text}</button>
+}
+
+Button.defaultProps = {
+  color: 'steelblue'
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
